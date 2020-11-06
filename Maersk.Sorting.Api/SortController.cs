587f42e4fa78ea795b090b6c50b26dd80c18837b
Burhanuddeen <maburhan@gmail.com>
2020-11-06 19:31:22 +0530

    1. Bubble Sorting Implemented 2. SRP Implemented. 3. Stack implemented for queue system

---
+++ Maersk.Sorting.Api/SortController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace Maersk.Sorting.Api.Controllers
+{
+    [ApiController]
+    [Route("sort")]
+    public class SortController : ControllerBase
+    {
+        private readonly ISortJobProcessor _sortJobProcessor;
+        private static StackSortJob processingJob = new StackSortJob();
+        private static StackSortJob completedJob = new StackSortJob();
+
+        public SortController(ISortJobProcessor sortJobProcessor)
+        {
+            _sortJobProcessor = sortJobProcessor;
+        }
+
+
+        [HttpPost("EnqueueJob")]
+        public async void EnqueueJob(int[] myNum)
+        { 
+
+            var pendingJob = new SortJob(
+                id: Guid.NewGuid(),
+                status: SortJobStatus.Pending,
+                duration: null,
+                input: myNum,
+                output: myNum);
+
+            processingJob.PushJobItem(pendingJob);
+
+            while (!processingJob.IsEmpty())
+            {
+                var processed = await _sortJobProcessor.Process(processingJob.RemoveSortJobItem());
+                completedJob.PushJobItem(processed);
+            }         
+        }
+
+        [HttpGet("GetJobs")]
+        public Task<List<SortJob>> GetJobs()
+        {
+            return Task.Run(() => {
+                List<SortJob> result = new List<SortJob>();
+                result.AddRange(processingJob.PrintStack());
+                result.AddRange(completedJob.PrintStack());
+                return result;
+            });
+           
+
+        }
+
+        [HttpGet("{jobId}")]
+        public Task<SortJob> GetJob(Guid jobId)
+        {
+            List<SortJob> result = new List<SortJob>();
+            result.AddRange(processingJob.PrintStack());
+            result.AddRange(completedJob.PrintStack());
+            
+            return Task.Run(() => {
+                return result.Where(it => it.Id == jobId).FirstOrDefault();
+                
+            });
+        }
+
+        public class StackSortJob
+        {
+            private const int MaxSize = 100;
+            private SortJob[] _items = new SortJob[MaxSize];
+            private int _currentIndex = -1;
+            public StackSortJob()
+            {
+            }
+            public void PushJobItem(SortJob element)
+            {
+                if (_currentIndex >= MaxSize - 1)
+                {
+                    throw new InvalidOperationException("Container is full");
+                }
+                _currentIndex++;
+                _items[_currentIndex] = element;
+            }
+            public SortJob RemoveSortJobItem()
+            {
+                if (IsEmpty())
+                {
+                    throw new InvalidOperationException("No elements Found");
+                }
+                SortJob element = _items[_currentIndex];
+                _currentIndex--;
+                return element;
+            }
+            public SortJob PeekSortJobItem()
+            {
+                if (IsEmpty())
+                {
+                    throw new InvalidOperationException("No elements found");
+                }
+                return _items[_currentIndex];
+            }
+            public bool IsEmpty()
+            {
+                if (_currentIndex < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public List<SortJob> PrintStack()
+            {
+                List<SortJob> result = new List<SortJob>();
+                if (_currentIndex < 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    
+                    for (int i = _currentIndex; i >= 0; i--)
+                    {
+                        result.Add(_items[_currentIndex]);
+
+                    }
+                    return result;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Maersk.Sorting.Api/SortJobProcessor.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maersk.Sorting.Api
+{
+    public class SortJobProcessor : ISortJobProcessor
+    {
+        private readonly ILogger<SortJobProcessor> _logger;
+
+        public SortJobProcessor(ILogger<SortJobProcessor> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<SortJob> Process(SortJob job)
+        {
+
+            _logger.LogInformation("Processing job with ID '{JobId}'.", job.Id);
+
+            var stopwatch = Stopwatch.StartNew();
+
+            int[] arr = new int[job.Input.Length];
+            Array.Copy(job.Input, 0, arr, 0, job.Input.Length);
+
+            this.Sort(job.Input);
+
+            await Task.Delay(5000); // NOTE: This is just to simulate a more expensive operation
+
+            var duration = stopwatch.Elapsed;
+
+            _logger.LogInformation("Completed processing job with ID '{JobId}'. Duration: '{Duration}'.", job.Id, duration);
+
+            return new SortJob(
+                id: job.Id,
+                status: SortJobStatus.Completed,
+                duration: duration,
+                input: arr,
+                output: job.Input);
+        }
+
+        //Implementing Bubble sort
+        private void Sort(int[] arr)
+        {
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+     
+    }
+}
